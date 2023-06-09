5bd2bb732334d39ff02621b9c73679df9c9c3f1e
Len Lee <clckflxh90@naver.com>
2023-06-10 01:08:54 +0900

    Lgg's Portfolio 꺠짐 현상 수정

--- src/section/stack/StackIcon.js
+++ src/section/stack/StackIcon.js
@@ -17,47 +17,16 @@ export default function StackIcon() {
         data-aos-duration={2000}
       >
         <div className={styles.skill_iconArea_1}>
-          <div
-            className={styles.iconHTML_2}
-            data-aos-anchors="#Stack"
-            data-aos="zoom-in"
-            data-aos-duration={1000}
-          ></div>
-          <h3
-            data-aos-anchors="#Stack"
-            data-aos="fade-left"
-            data-aos-duration={1000}
-          >
-            HTML
-          </h3>
+          <div className={styles.iconHTML_2}></div>
+          <h3>HTML</h3>
         </div>
         <div className={styles.skill_iconArea_2}>
-          <h3
-            data-aos-anchors="#Stack"
-            data-aos="fade-right"
-            data-aos-duration={1000}
-          >
-            CSS
-          </h3>
-          <div
-            className={styles.iconCSS_2}
-            data-aos-anchors="#Stack"
-            data-aos="zoom-in"
-            data-aos-duration={1000}
-          ></div>
+          <h3>CSS</h3>
+          <div className={styles.iconCSS_2}></div>
         </div>
         <div className={styles.skill_iconArea_3}>
-          <div
-            className={styles.iconJS_2}
-            data-aos-anchors="#Stack"
-            data-aos="zoom-in"
-            data-aos-duration={1000}
-          ></div>
-          <h3
-            data-aos-anchors="#Stack"
-            data-aos="fade-left"
-            data-aos-duration={1000}
-          >
+          <div className={styles.iconJS_2}></div>
+          <h3>
             Java <br />
             Script
           </h3>
@@ -74,17 +43,8 @@ export default function StackIcon() {
           className={styles.skill_iconArea_1}
           style={{ backgroundColor: "#9AE1F7" }}
         >
-          <div
-            className={styles.iconReactJS_2}
-            data-aos-anchors="#Stack"
-            data-aos="zoom-in"
-            data-aos-duration={1000}
-          ></div>
-          <h3
-            data-aos-anchors="#Stack"
-            data-aos="fade-left"
-            data-aos-duration={1000}
-          >
+          <div className={styles.iconReactJS_2}></div>
+          <h3>
             React <br />
             JS
           </h3>
@@ -93,20 +53,11 @@ export default function StackIcon() {
           className={styles.skill_iconArea_2}
           style={{ backgroundColor: "#78C1DE" }}
         >
-          <h3
-            data-aos-anchors="#Stack"
-            data-aos="fade-right"
-            data-aos-duration={1000}
-          >
+          <h3>
             React <br />
             Native
           </h3>
-          <div
-            className={styles.iconRN_2}
-            data-aos-anchors="#Stack"
-            data-aos="zoom-in"
-            data-aos-duration={1000}
-          ></div>
+          <div className={styles.iconRN_2}></div>
         </div>
       </div>
 
@@ -120,38 +71,15 @@ export default function StackIcon() {
           className={styles.skill_iconArea_1}
           style={{ backgroundColor: "#7FDAAD" }}
         >
-          <div
-            className={styles.iconFigma_2}
-            data-aos-anchors="#Stack"
-            data-aos="zoom-in"
-            data-aos-duration={1000}
-          ></div>
-          <h3
-            data-aos-anchors="#Stack"
-            data-aos="fade-left"
-            data-aos-duration={1000}
-          >
-            Figma
-          </h3>
+          <div className={styles.iconFigma_2}></div>
+          <h3>Figma</h3>
         </div>
         <div
           className={styles.skill_iconArea_2}
           style={{ backgroundColor: "rgb(66, 66, 66)" }}
         >
-          <h3
-            style={{ color: "white" }}
-            data-aos-anchors="#Stack"
-            data-aos="fade-right"
-            data-aos-duration={1000}
-          >
-            Git Hub
-          </h3>
-          <div
-            className={styles.iconGit_2}
-            data-aos-anchors="#Stack"
-            data-aos="zoom-in"
-            data-aos-duration={1000}
-          ></div>
+          <h3 style={{ color: "white" }}>Git Hub</h3>
+          <div className={styles.iconGit_2}></div>
         </div>
       </div>
     </div>
